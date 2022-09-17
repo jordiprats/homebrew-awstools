29f9db550954e5dc4eeb93fee78e41eb58d5a13d
Jordi Prats <jordi.prats@gmail.com>
2022-09-17 13:02:19 +0200

    Update awstools.rb

--- awstools.rb
+++ awstools.rb
@@ -1,7 +1,7 @@
 class Awstools < Formula
   homepage "https://github.com/jordiprats/awstools"
   url "https://github.com/jordiprats/awstools/archive/refs/tags/1.0.tar.gz"
-  sha256 "0824fb1c906d87a82bd35e48be9f8aa616dc7e8ba227091691af514e8a60cebf"
+  sha256 "ed3d2d9d0c46dd25d099d092ad6cd68f738c0b20c0f4d6d612534a89d0f2f608"
 
   def install
     system "python3", "-m", "pip", "install", "-r", "requirements.txt"
